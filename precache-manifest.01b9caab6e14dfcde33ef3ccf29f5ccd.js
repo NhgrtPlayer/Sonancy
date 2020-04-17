95b60a86c8687a0e0d831361e589c80496524e16
NhgrtPlayer <nhgrtplayer@hotmail.fr>
2020-04-17 14:37:11 +0200

    Updates

---
+++ precache-manifest.01b9caab6e14dfcde33ef3ccf29f5ccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db098b085df47258ac9baec53646326f",
+    "url": "/Sonancy/index.html"
+  },
+  {
+    "revision": "405715acd724ceafa7da",
+    "url": "/Sonancy/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "53704cace5043a348860",
+    "url": "/Sonancy/static/js/2.f82f0d2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sonancy/static/js/2.f82f0d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405715acd724ceafa7da",
+    "url": "/Sonancy/static/js/main.c0a3d859.chunk.js"
+  },
+  {
+    "revision": "eb1cbe9869957257e665",
+    "url": "/Sonancy/static/js/runtime-main.9cf60c50.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Sonancy/static/media/logo.ee7cd8ed.svg"
+  }
+]);
