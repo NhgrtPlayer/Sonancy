04c14a113f3c8cbc514955689e575239cb76f803
NhgrtPlayer <nhgrtplayer@hotmail.fr>
2020-04-23 00:45:41 +0200

    Login header & drawer

---
+++ src/header/Header.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import HeaderRoomSelector from './HeaderRoomSelector';
+import HeaderLoginForm from './HeaderLoginForm';
+import HeaderSignupForm from './HeaderSignupForm';
+
+export default function Header() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [loginDrawerOpen, setLoginDrawerOpen] = React.useState(false);
+    const [signupDrawerOpen, setSignupDrawerOpen] = React.useState(false);
+  
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const openDrawer = (isLoginDrawer) => {
+      setLoginDrawerOpen(isLoginDrawer);
+      setSignupDrawerOpen(!isLoginDrawer);
+    }
+
+    const closeDrawer = () => {
+      setLoginDrawerOpen(false);
+      setSignupDrawerOpen(false);
+    }
+
+    return (
+        <header>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="title" color="inherit" className={classes.title}>
+                  SONANCY
+              </Typography>
+              <Button
+              variant="contained"
+              color="primary"
+              onClick={handleClick}>
+                  <div>
+                      JOIN ROOM
+                      <div>
+                          No signup required !
+                      </div>
+                  </div>
+              </Button>
+              <HeaderRoomSelector
+              open={Boolean(anchorEl)}
+              anchorEl={anchorEl}
+              handleClose={handleClose}
+              />
+              <Button
+              variant="contained"
+              color="primary"
+              onClick={() => openDrawer(true)}>
+                  LOGIN
+              </Button>
+              <Button
+              variant="contained"
+              color="primary"
+              onClick={() => openDrawer(false)}>
+                  SIGNUP
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Drawer anchor={'right'} open={loginDrawerOpen || signupDrawerOpen} onClose={() => closeDrawer()}>
+            {loginDrawerOpen &&
+            <HeaderLoginForm/>
+            }
+            {signupDrawerOpen &&
+            <HeaderSignupForm/>
+            }
+          </Drawer>
+        </header>
+    )
+}
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+}));
---
+++ src/header/HeaderRoomSelector.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Popover from '@material-ui/core/Popover';
+import TextField from '@material-ui/core/TextField';
+
+const ROOM_CODE_LENGTH = 4;
+
+export default function HeaderRoomSelector({open, anchorEl, handleClose}) {
+    const [roomId, setRoomId] = React.useState(undefined);
+    const [error, setError] = React.useState(false);
+    //const router = useRouter();
+
+    const handleChange = (event) => {
+        const input = event.target.value;
+        const value = input.replace(/\D+/g,'').substring(0, ROOM_CODE_LENGTH); //Remove all non-digits characters
+        if (isNaN(parseInt(value))) {
+            setRoomId(undefined);
+        } else {
+            setRoomId(value);
+        }
+        setError(checkError(value));
+    };
+
+    const checkError = (value) => {
+        if (value && value.toString().length !== ROOM_CODE_LENGTH) {
+            return (true);
+        }
+        return (false);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (error) {
+            return;
+        }
+        handleClose();
+        //Check si la room existe ici
+        //router.push('/room/' + roomId);
+    }
+
+    return ( 
+    <Popover
+        id='header-room-selector'
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+        }}
+        transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+        }}
+    >
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField
+                id="filled-basic"
+                variant="outlined"
+                placeholder="0000"
+                size="small"
+                value={roomId}
+                onChange={handleChange}
+                error={error}
+            />
+        </form>
+    </Popover>
+    );
+}
