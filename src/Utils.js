69a4333f17b0fa46633d7f878dc4a63131dc0d19
NhgrtPlayer <nhgrtplayer@hotmail.fr>
2020-05-07 18:36:49 +0200

    reset

--- src/Utils.js
+++
@@ -1,7 +0,0 @@
-export default class Utils {
-    static getRandomNumber(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-}
--- src/header/HeaderSignupForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HeaderSignupForm() {
-    return (
-        <div>
-            SIGNUP
-        </div>
-    );
-}
